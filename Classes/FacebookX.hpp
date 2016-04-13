0d9eae2e42cc4d108f026427c22dc32d659088a6
EL CAPITAN <tranhoangduong1994@gmail.com>
2016-04-13 12:22:37 +0700

    write code get access token, get user id, is logged in, logout for iOS

--- Classes/FacebookX.hpp
+++ Classes/FacebookX.hpp
@@ -10,8 +10,23 @@
 #define FacebookX_hpp
 
 #include <string>
+#include <vector>
 
 namespace h102 {
+  
+  const std::string FB_PERM_READ_PUBLIC_PROFILE("public_profile");
+  const std::string FB_PERM_READ_EMAIL("email");
+  const std::string FB_PERM_READ_USER_FRIENDS("user_friends");
+  const std::string FB_PERM_PUBLISH_POST("publish_actions");
+  const std::string FB_API_ME_FRIENDS_TAG("__fb_me_friends__");
+  const std::string FB_API_ME_FRIENDS_PATH("me/friends");
+  
+  const std::string kRI_ExcludeFromList("ExcludeFromListIds");
+  const std::string kRI_PictureSize("PictureSize");
+  const std::string kRI_PaginationLimit("PaginationLimit");
+  const std::string kRI_ResponseFields("ResponseFields");
+  
+  
   class FacebookListener
   {
   public:
@@ -30,9 +45,19 @@ namespace h102 {
   };
   
   class FacebookX {
+    
+      static FacebookListener* listener;
+    
   public:
-    static void init();
-    static void setListener(FacebookListener* listener);
+      static void init();
+      static void setListener(FacebookListener* listener);
+    
+      static void login();
+      static void login( std::vector<std::string>& permissions );
+      static std::string getAccessToken();
+      static std::string getUserID();
+      static bool isLoggedIn();
+      static void logout();
   };
 }
 
--- platforms/ios/FacebookX.mm
+++ platforms/ios/FacebookX.mm
@@ -1,9 +1,72 @@
 //
 //  FacebookX.m
-//  FacebookXTest
+//  ;
 //
 //  Created by Thuy Dong Xuan on 4/12/16.
 //
 //
 
 #import <Foundation/Foundation.h>
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#import "UIViewController+Utils.h"
+
+#import "FacebookX.hpp"
+
+using namespace std;
+
+namespace h102 {
+	void FacebookX::login() {
+		vector<string> permissions;
+		permissions.push_back(h102::FB_PERM_READ_PUBLIC_PROFILE);
+		login(permissions);
+  	}
+  
+  	void FacebookX::login( std::vector<std::string>& permissions ) {
+		FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+	
+		NSMutableArray *permissionArray = [NSMutableArray new];
+		for (auto str : permissions) {
+	  		id nsstr = [NSString stringWithUTF8String:str.c_str()];
+	  		[permissionArray addObject:nsstr];
+		}
+	
+		[login logInWithReadPermissions: permissionArray
+				fromViewController:[UIViewController topViewController]
+							handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+			if (error) {
+	//         	NSLog(@"Process error");
+				if (FacebookX::listener) {
+					listener->onLogin(false, error.localizedDescription.UTF8String);
+				}
+			} else if (result.isCancelled) {
+	//         	NSLog(@"Cancelled");
+				if (FacebookX::listener) {
+				   	listener->onLogin(false, "Cancelled");
+				}
+		   	} else {
+                [FBSDKProfile enableUpdatesOnAccessTokenChange:YES];
+				listener->onLogin(true, "LoggedIn");
+			}
+		}];
+  	}
+
+    std::string FacebookX::getAccessToken() {
+        return std::string([[[FBSDKAccessToken currentAccessToken] tokenString] UTF8String]);
+    }
+    
+    std::string FacebookX::getUserID() {
+        auto profile = [FBSDKProfile currentProfile];
+        return std::string([[FBSDKProfile currentProfile].userID UTF8String]);
+    }
+    
+    bool FacebookX::isLoggedIn() {
+        return [[FBSDKAccessToken currentAccessToken] tokenString] != nil;
+    }
+    
+    void FacebookX::logout() {
+        [[[FBSDKLoginManager alloc] init] logOut];
+    }
+}
